51dc7c65f38be08ab8904251eeac86d6d399091f
FachryAlhasani <@FachryAlhasani17>
2025-04-28 21:02:29 +0700

    reponya telat huuu

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Events\MessageSent;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/send-message', function () {
+    broadcast(new MessageSent('Hello, this is a test message!'));
+    return 'Message sent!';
 });
